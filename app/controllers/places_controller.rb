fc2b66eba47b870d5d6a37e62590a0e5d9b673ca
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-02-19 02:45:26 +0000

    hw3_progress

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,30 +1,30 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all 
-    # find all Company rows
+    # find all place rows
     # render companies/index view
   end
 
    def show
-     # find a Company
+     # find a place
      @place = Place.find_by({"id" => params["id"]})
-     # render companies/show view with details about Company
+     # render companies/show view with details about place
    end
 
    def new
-   # render view with new Company form
+   # render view with new place form
    @place = Place.new
     # render posts/new view with new Post form
    end
 
   def create
-     # start with a new Company
+     # start with a new place
      @place = Place.new
-     # assign user-entered form data to Company's columns
+     # assign user-entered form data to Place's columns
      @place["name"] = params["name"]
-     @place["activity"] = params["activity"]
-     @place["time_stamp"] = params["time_stamp"]
-     # save Company row
+     @place["created_at"] = params["created_at"]
+     @place["updated_at"] = params["upadted_at"]
+     # save Place row
      @Place.save
      # redirect user
      redirect_to "/places"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,12 @@
 class Place < ApplicationRecord
   # talks to the "places" table
+ 
+  # create input for Peru
+  peru = Place.new
+peru["name"] = "Peru"
+peru["created_at"] = "12-12-2023"
+peru["updated_at"] = "12-24-2023"
+peru.save
+
+puts "There are #{Place.all.count} places."
 end
