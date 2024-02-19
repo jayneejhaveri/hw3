15182bcbd5738e24eebc212c3668ada8196288d1
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-02-19 03:19:51 +0000

    hw3_progress

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,31 @@
 class Place < ApplicationRecord
   # talks to the "places" table
+
+  # Delete existing data, so you'll start fresh each time this script is run.
+# Use `Model.destroy_all` code.
+# TODO!
+Place.destroy_all
+
  
   # create input for Peru
   peru = Place.new
 peru["name"] = "Peru"
 peru["created_at"] = "12-12-2023"
 peru["updated_at"] = "12-24-2023"
+peru.save
+
+  # create input for Columbia
+  peru = Place.new
+peru["name"] = "Columbia"
+peru["created_at"] = "12-08-2023"
+peru["updated_at"] = "12-11-2023"
+peru.save
+
+  # create input for South Africa
+  peru = Place.new
+peru["name"] = "South Africa"
+peru["created_at"] = "08-12-2023"
+peru["updated_at"] = "08-24-2023"
 peru.save
 
 puts "There are #{Place.all.count} places."
--- db/migrate/20220210225337_create_places.rb
+++ db/migrate/20220210225337_create_places.rb
@@ -2,7 +2,8 @@ class CreatePlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :places do |t|
       t.string "name"
-
+      t.string "created_at"
+      t.string "updated_at"
       t.timestamps
     end
   end
