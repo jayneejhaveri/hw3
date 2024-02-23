8cf092178316ffc7a45dae87f19c336b77ceb6ec
jayneejhaveri <jaynee.jhaveri@kellogg.northwestern.edu>
2024-02-23 01:16:01 +0000

    hw_3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,16 @@
 class EntriesController < ApplicationController
   
-   def show
+  #def show
      # find a activity
-     @entry = Entry.find_by({ "id" => params["id"] })
-     @place = Place.find_by({"id" => @entry["place_id"]})
+     #@entry = Entry.find_by({ "id" => params["id"] })
+     #@place = Place.find_by({"id" => @entry["place_id"]})
      # render companies/show view with details about place
-   end
+  #end
 
    def new
    # render view with new place form
-   @place = Place.find_by({"id" => @entry["place_id"]})
+    @entry = Entry.new
+    @place = Place.find_by({"id" => params["place_id"]})
     # render posts/new view with new Post form
    end
 
@@ -21,13 +22,12 @@ class EntriesController < ApplicationController
      @entry["description"] = params["description"]
      @entry["posted_on"] = params["posted_on"]
      @entry["place_id"] = params["place_id"]
-     @entry["created_at"] = params["created_at"]
-     @entry["updated_at"] = params["updated_at"]
+
 
          # assign relationship between Contact and Company
      @entry["place_id"] = params["place_id"]
      # save Place row
-     @Entry.save
+     @entry.save
 
      # redirect user
      redirect_to "/places/#{@entry["place_id"]}"
